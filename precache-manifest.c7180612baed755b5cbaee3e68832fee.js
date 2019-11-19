2fd4584a56c1c74f92c485349f07d9af1c766097
Tanpa Nama <alsigitguntoro@gmail.com>
2019-11-19 09:31:06 +0700

    mantab

---
+++ precache-manifest.c7180612baed755b5cbaee3e68832fee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c1b2db92df1dfb992f",
+    "url": "/css/about.62814ed6.css"
+  },
+  {
+    "revision": "25bb27c0f5a0ca0859b1",
+    "url": "/css/chunk-vendors.583c33a2.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b1dc24358006698b764644e00ec49eef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02c1b2db92df1dfb992f",
+    "url": "/js/about.a071b2fa.js"
+  },
+  {
+    "revision": "6e74dd42d1f840f0ef1c",
+    "url": "/js/app.a1c27c29.js"
+  },
+  {
+    "revision": "25bb27c0f5a0ca0859b1",
+    "url": "/js/chunk-vendors.8231bd84.js"
+  },
+  {
+    "revision": "574b1a5db6bd6a29d48b3b2692f55196",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
